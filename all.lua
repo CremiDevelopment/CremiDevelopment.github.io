ff5c68e9c0e8aae90858310661b6b8898fbf454b
CremiWorld <89044831+CremiWorld@users.noreply.github.com>
2021-08-17 13:33:35 +0300

    Update and rename all to all.lua

---
+++ all.lua
@@ -0,0 +1,35 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/menu.lua"))();
+
+
+local Gamer = library:CreateSection("Shindo Life");
+Gamer:Button("~ Нажмите Правый Шифт чтобы скрыть Интерфейс ~");
+function LoadMe(link)
+    loadstring(game:HttpGet(link))()
+end
+Gamer:Button("MheeHub", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/cheats/MheeHub.lua",true))()
+end)
+Gamer:Button("Скрипт 2 ReavScript", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/reavscripts/sl2_autofarm/main/main.lua", true))()
+end)
+Gamer:Button("Скрипт 3", function()
+    LoadMe('https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/cheats/Cheat2.lua')
+end)
+Gamer:Button("Скрипт 4", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/cheats/Cheat3.lua'),true))()
+end)
+Gamer:Button("BruhHub", function()
+	loadstring(game:HttpGet('https://bruh.keshhub.com/.lua'))()
+end)
+Gamer:Button("Авто Квест", function()
+    LoadMe('https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/cheats/AutoQuest.lua')
+end)
+Gamer:Button("Авто КГ", function()
+    LoadMe('https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/cheats/AutoKG.lua')
+end)
+Gamer:Button('Бесконечная Чакра',function()
+    _G.InfChakra = true
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/CremiWorld/RobloxCheat_By_Cremi/main/cheats/InfinityChakra.lua'))()    
+end)
+
+library:Ready();
