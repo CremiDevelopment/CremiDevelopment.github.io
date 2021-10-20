96259515d62ebbe6e4b24093fdd50d486504bcf8
CremiWorld <89044831+CremiWorld@users.noreply.github.com>
2021-10-20 20:12:55 +0300

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,25 +1,25 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/menu.lua"))();
-
-
 local Gamer = library:CreateSection("Shindo Life");
 Gamer:Button("~ Нажмите Правый Шифт чтобы скрыть Интерфейс ~");
 function LoadMe(link)
     loadstring(game:HttpGet(link))()
-end)
+end
 Gamer:Button("SnowHub", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/SecretFolder/Web/scripts/SnxwHub.txt", true))()
 end)
 Gamer:Button("BruhHub", function()
 	loadstring(game:HttpGet('https://bruh.keshsenpai.com/.lua'))()
 end)
-Gamer:Button("Kn0wHub", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/SecretFolder/Web/scripts/KnwHub.txt", true))()
-end)
 Gamer:Button("Скрипт 1 ReavScript", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/reavscripts/sl2_autofarm/main/main.lua", true))()
+end)
+Gamer:Button("Скрипт 2", function()
+    LoadMe('https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/SecretFolder/Web/scripts/KnwHub.txt')
+end)
+Gamer:Button("Скрипт 3", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/cheats/Cheat3.lua'),true))()
 end)
 Gamer:Button("Bloodline Changer", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xzqecc/Script/main/xzqec_changer"))()
 end)
-
 library:Ready();
