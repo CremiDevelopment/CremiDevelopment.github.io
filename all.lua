83ba1ef7c7eaa84a75ec9ceea7514d50a66e0eb8
CremiWorld <89044831+CremiWorld@users.noreply.github.com>
2021-10-20 18:12:24 +0300

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,16 +1,25 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/menu.lua"))();
+local Remote = game.ReplicatedStorage:WaitForChild("Remote Event Name Here")
 
 
 local Gamer = library:CreateSection("Shindo Life");
 Gamer:Button("~ Нажмите Правый Шифт чтобы скрыть Интерфейс ~");
 function LoadMe(link)
     loadstring(game:HttpGet(link))()
-end
+end)
+Gamer:Button("Destroi Gui", function()
+	Remote.OnServerEvent:Connect(function(player)
+	    Remote:FireAllClients()
+	end)
+end)
 Gamer:Button("SnowHub", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/SecretFolder/Web/scripts/SnxwHub.txt", true))()
 end)
 Gamer:Button("BruhHub", function()
 	loadstring(game:HttpGet('https://bruh.keshsenpai.com/.lua'))()
+end)
+Gamer:Button("Kn0wHub", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CremiWorld/CremiWorld.github.io/main/SecretFolder/Web/scripts/KnwHub.txt", true))()
 end)
 Gamer:Button("Скрипт 1 ReavScript", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/reavscripts/sl2_autofarm/main/main.lua", true))()
