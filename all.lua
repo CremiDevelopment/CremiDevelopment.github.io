04a5902fd7b1bd88a691687586f8360bdf437792
CremiWorld <89044831+CremiWorld@users.noreply.github.com>
2022-02-07 10:03:59 +0200

    Update all.lua

--- all.lua
+++ all.lua
@@ -3,7 +3,6 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Cremi
 
 local Gamer = library:CreateSection("Shindo Life");
 Gamer:Button("~ Нажмите Правый Шифт чтобы скрыть Интерфейс ~");
-
 function LoadMe(link)
     loadstring(game:HttpGet(link))()
 end
