f05a60a4cb4f0636bab3669e5f01ef05a3c7ae51
CremiWorld <89044831+CremiWorld@users.noreply.github.com>
2022-04-11 16:09:50 +0300

    Update key.php

--- key.php
+++ key.php
@@ -1,14 +1,4 @@
 <?php
-// create a new cURL resource
-$ch = curl_init();
-
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, "https://626e-128-124-253-87.ngrok.io/");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// grab URL and pass it to the browser
-curl_exec($ch);
-
-// close cURL resource, and free up system resources
-curl_close($ch);
+$html = file_get_html('https://626e-128-124-253-87.ngrok.io/');
+echo $html;
 ?>
