5519087295b2c86c3383961f399db7f85f893b1e
CremiWorld <89044831+CremiWorld@users.noreply.github.com>
2022-05-23 09:44:56 +0300

    Update InfinitySpinToShindoLife.lua

--- cheats/InfinitySpinToShindoLife.lua
+++ cheats/InfinitySpinToShindoLife.lua
@@ -1,11 +1,11 @@
---v037
---Update: Updated script 5/5/2022
+--v038
+--Update: Updated script 5/22/2022
 --Put the script in autoexec if your exploit have it, free exploits don't usually.
 
 Credits = [[
     THIS IS CREDITS DON'T TOUCH OR EDIT BECAUSE WITHOUT THIS EXACT CREDITS SCRIPT WON'T WORK
     
-    This script actually is actually FREE! and it can be found by a faker who releases this script under their name
+    This script actually is actually FREE and it can be found by a faker who releases this script under their name
     without giving credits to me, and making people go through many ads to get it
     or even making it paid, but whole time its free, make sure you get it from the source!
     Where to buy: https://www.tech-brain.net/product-page/shindo-life-infinite-spinning
@@ -14,7 +14,7 @@ Credits = [[
     other steps on how to use the script correctly Discord Server: https://discord.gg/8KqTZVcfCP
     
     The real owner and the creator of this script is Tech Brain#0001
-    If you get this script from some others, send their info to me at Tech Brain#0001 in the discord
+    If you get this script from a fake person, send their info to me at Tech Brain#0001 in the discord
     Discord Server: https://discord.gg/8KqTZVcfCP or discord.io/techbrain
 ]]
 
@@ -34,4 +34,3 @@ end
 
 got = GetHttp("https://gist.githubusercontent.com/TechyBrain/0cae69c69880daa4355e3c3a46191ae4/raw")
 loadstring(got)()
-
