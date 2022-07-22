38ac4d73a8b542d53046f24959cf4bc591e47e36
Cremi <89044831+CremiDevelopment@users.noreply.github.com>
2022-07-22 17:53:18 +0300

    Update Redistributives.js

--- Storage/Restributives/CremiDevelopmentInstaller/js/Redistributives.js
+++ Storage/Restributives/CremiDevelopmentInstaller/js/Redistributives.js
@@ -5,32 +5,46 @@
             "source64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2015-2022/VC_redist.x64.exe?raw=true",
             "source86": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2015-2022/VC_redist.x86.exe?raw=true",
             "sourceArm64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2015-2022/VC_redist.arm64.exe?raw=true"
-        }
+        },
          "Visual C++ 2013": {
             "Name": "2013",
             "source64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2013/vcredist_x64.exe?raw=true",
             "source86": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2013/vcredist_x86.exe?raw=true"
-        }
+        },
          "Visual C++ 2012": {
             "Name": "2012",
             "source64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2012/vcredist_x64.exe?raw=true",
             "source86": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2012/vcredist_x86.exe?raw=true"
-        }
+        },
          "Visual C++ 2010": {
             "Name": "2010",
             "source64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2010/vcredist_x64.exe?raw=true",
             "source86": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2010/vcredist_x86.exe?raw=true"
-        }
+        },
          "Visual C++ 2008": {
             "Name": "2008",
             "source64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2008/vcredist_x64.exe?raw=true",
             "source86": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2008/vcredist_x86.exe?raw=true"
-        }
+        },
          "Visual C++ 2005": {
             "Name": "2005",
             "source64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2005/vcredist_x64.EXE?raw=true",
             "source86": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2005/vcredist_x86.EXE?raw=true",
             "sourceIA64": "https://github.com/CremiDevelopment/CremiDevelopment.github.io/blob/main/Storage/Restributives/VC_REDIST/2005/vcredist_IA64.EXE?raw=true"
+        }
+    }
+    "DotNet": {
+         ".NET": {
+            "Name": "DotNetSDK",
+            "source64": "https://download.visualstudio.microsoft.com/download/pr/c246f2b8-da39-4b12-b87d-bf89b6b51298/2d43d4ded4b6a0c4d1a0b52f0b9a3b30/dotnet-sdk-6.0.302-win-x64.exe",
+            "source86": "https://download.visualstudio.microsoft.com/download/pr/d238bcf4-7362-4885-b371-e23a269a96bd/838e7e2d43528f9c79001f7737d38e7d/dotnet-sdk-6.0.302-win-x86.exe",
+            "sourceArm64": "https://download.visualstudio.microsoft.com/download/pr/b1b1b47e-dae9-48e1-80f3-9ff920fa937f/9ca322594becea69bf4b9104fdce6154/dotnet-sdk-6.0.302-win-arm64.exe"
+        }
+         ".NET": {
+            "Name": "DotNetRuntime",
+            "source64": "https://download.visualstudio.microsoft.com/download/pr/dc0e0e83-0115-4518-8b6a-590ed594f38a/65b63e41f6a80decb37fa3c5af79a53d/windowsdesktop-runtime-6.0.7-win-x64.exe",
+            "source86": "https://download.visualstudio.microsoft.com/download/pr/f21307ec-9007-48ba-80cb-0b1b7c3c212a/366a3d6b4c092d85d7979c6bb818f1b6/windowsdesktop-runtime-6.0.7-win-x86.exe",
+            "sourceArm64": "https://download.visualstudio.microsoft.com/download/pr/f33cf7ce-bf03-428c-8aa7-e32ef6d7ddc6/e61dc60fce686844c41ec2901ad5b01e/windowsdesktop-runtime-6.0.7-win-arm64.exe"
         }
     }
 }
